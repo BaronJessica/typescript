a19710664a025455aba4086179a2da5d89fa5636
BaronJessica <jessicabaron93@gmail.com>
2023-01-30 14:05:46 -0300

    TypeScript lessons

---
+++ aula1/src/A01-type-annotation/A01-type-annotation.ts
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+
+let nome: string = 'luiz';
+
+
+//arrays
+let arrayDeNumeros: Array<number> = [1,23,23,11,13];
+let arrayDeNumeros2: number[] = [1,23,23,11,13];
+let arrayDeStrings: Array<string> = ['1,23,23,11,13', 'eee'];
+let arrayDeStrings2: string[] = ['1,23,23,11,13', 'eee'];
+
+//objetos
